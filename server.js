ef1eee182d9aaaf854161f6f81537b7d44569539
yurika sakae <yurika.sun@gmail.com>
2019-09-12 16:19:10 -0700

    seed created

--- server.js
+++ server.js
@@ -12,4 +12,8 @@ server.get("/", (req, res) => {
   res.send("API up");
 });
 
+server.get("/api/recipe", (req, res) => {
+  db("recipe").then(recipes => res.status(200).json(recipes));
+});
+
 module.exports = server;
